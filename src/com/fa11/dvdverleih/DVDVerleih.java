338ff071e70feb4cfe28dbe316a091922b3576b7
Dirtbiker <marzocchi_bomber90@yahoo.de>
2013-08-29 12:06:20 +0200

    Erstuebergabe

---
+++ src/com/fa11/dvdverleih/DVDVerleih.java
@@ -0,0 +1,17 @@
+package com.fa11.dvdverleih;
+/**
+ * Kurzbeschreibung der Klasse
+ * @version 1.0 vom 29.08.2013
+ * @author Manfred Mustermann
+ */
+public class DVDVerleih {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
