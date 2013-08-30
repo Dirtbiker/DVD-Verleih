e2df57476e0216b7bed50a1c20abca2a73ce1256
Timo Raschke <timo.raschke@gmail.com>
2013-08-30 15:14:00 +0200

    GUI und TUI Klassen erstellt

---
+++ DVD-Verleih/src/com/fa11/dvdverleih/ui/GUI.java
@@ -0,0 +1,48 @@
+package com.fa11.dvdverleih.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * Grafische Benutzeroberfläche für den DVD-Verleih mit Swing.
+ * @author Timo Raschke
+ */
+public class GUI extends JFrame {
+
+	private static final long serialVersionUID = -3004396365580658126L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setTitle("DVD-Verleih");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ DVD-Verleih/src/com/fa11/dvdverleih/ui/TUI.java
@@ -0,0 +1,12 @@
+package com.fa11.dvdverleih.ui;
+
+/**
+ * Command Line Interface für den DVD-Verleih.
+ * @author Timo Raschke
+ *
+ */
+public class TUI {
+	public TUI(){
+		
+	}
+}
