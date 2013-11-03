b9b91cfed906c541c6e79e6bd0ba6d0841e7ab95
Timo Raschke <timo.raschke@gmail.com>
2013-11-03 15:28:28 +0100

    Add: StarterGUI starten

--- DVD-Verleih/src/com/fa11/dvdverleih/DVDVerleih.java
+++ DVD-Verleih/src/com/fa11/dvdverleih/DVDVerleih.java
@@ -1,8 +1,28 @@
 package com.fa11.dvdverleih;
 
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+
+import com.fa11.dvdverleih.ui.StarterGUI;
+
 public class DVDVerleih {
 
 	public static void main(String[] args) {
-		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StarterGUI frame = new StarterGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
